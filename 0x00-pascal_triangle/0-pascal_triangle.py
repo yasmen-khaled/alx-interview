83b7231027a66ea74d1ef7d65727428f1d733974
yasmen <yasmen.khaled2000@gmail.com>
2024-04-25 05:24:29 +0200

    done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""python func for  Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    while len(triangle) != n:
+        previous = triangle[-1]
+        current = [1]
+        for i in range(len(previous) - 1):
+            current.append(previous[i] + previous[i + 1])
+        current.append(1)
+        triangle.append(current)
+    return triangle
