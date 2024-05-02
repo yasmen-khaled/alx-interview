bee876453761eca8873f14c79be969332346890c
yasmen <yasmen.khaled2000@gmail.com>
2024-05-02 21:26:58 +0200

    donme

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """
-You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 
+You have n number of locked boxes in front of you. Each box is numbered
+sequentially from 0 to n - 1
 and each box may contain keys to the other boxes.
     """
+
+
 def canUnlockAll(boxes):
     """checks"""
     if (not isinstance(boxes, list)):
@@ -10,16 +13,13 @@ def canUnlockAll(boxes):
 
     seen = [False for _ in range(len(boxes))]
     seen[0] = True
-
-    
     useBFS(boxes, seen)
-
-    
     return all(seen)
+
 
 def useBFS(boxes, seen):
     """uses iteration to visit nodes breadth-first
-        
+
     """
     for i, box in enumerate(boxes):
         if (isinstance(box, list) is not True):
@@ -29,7 +29,7 @@ def useBFS(boxes, seen):
             if (isinstance(key, int) is not True):
                 continue
 
-            # guard against 
+            # guard against
             if (key >= len(boxes)):
                 continue
 
@@ -37,6 +37,7 @@ def useBFS(boxes, seen):
                 continue
 
             seen[key] = True
+
 
 def useDFS(boxes, seen, currentBoxIdx):
     """uses recursion to visit nodes depth-first
